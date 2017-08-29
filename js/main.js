87e61fffedefcadc89282ce17e9757059282abfa
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-08-29 15:08:40 -0700

    operator buttons

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 var readout = document.querySelector('#readout');
 var digits = document.querySelectorAll('.digit');
+var operators = document.querySelectorAll('.operator');
+var currentOperator = '';
 
 // for each digit, do a function
-digits.forEach(function(btn){
+digits.forEach(function(btn) {
   // add an event listener to each function
   btn.addEventListener('click', function() {
     // 'this' is the button being clicked...
     readout.innerText += this.innerText;
   });
 });
+
+operators.forEach(function(operator) {
+  operator.addEventListener('click', function(){
+    // set current operator
+    if(this.innerText === 'x'){
+      currentOperator = '*';
+    }
+    else {
+      currentOperator = this.innerText;
+    }
+  })
+})
