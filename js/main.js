7e353588c0078af168441014d8d65997a4d3e7cb
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-08-29 14:55:19 -0700

    event listeners for digits

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-console.log('loaded')
+var readout = document.querySelector('#readout');
+var digits = document.querySelectorAll('.digit');
+
+// for each digit, do a function
+digits.forEach(function(btn){
+  // add an event listener to each function
+  btn.addEventListener('click', function() {
+    // 'this' is the button being clicked...
+    readout.innerText += this.innerText;
+  });
+});
