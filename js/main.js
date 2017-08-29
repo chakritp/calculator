fdb91d6d44e97d77ce58291cda34694fa5f5c042
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-08-29 15:55:35 -0700

    Create basic evaluate function to work with equals sign

--- js/main.js
+++ js/main.js
@@ -3,29 +3,62 @@ var digits = document.querySelectorAll('.digit');
 var operators = document.querySelectorAll('.operator');
 var currentOperator = '';
 var equalsBtn = document.querySelector('#equals');
+var firstNumber = 0;
+var secondNumber = 0;
+var operatorJustClicked = false;
+
+function evaluate(operation, firstNumber, secondNumber) {
+  switch(operation){
+    case '+':
+      return firstNumber + secondNumber;
+    case '-':
+      return firstNumber - secondNumber;
+    case '*':
+      return firstNumber * secondNumber;
+    case '/':
+      return firstNumber / secondNumber;
+  }
+}
 
 // for each digit, do a function
 digits.forEach(function(btn) {
   // add an event listener to each function
   btn.addEventListener('click', function() {
     // 'this' is the button being clicked...
+    if(operatorJustClicked){
+      readout.innerText = ''; // clear the readout when someone just clicked an opreator
+      operatorJustClicked = false;
+    }
     readout.innerText += this.innerText;
   });
 });
 
 operators.forEach(function(operator) {
   operator.addEventListener('click', function(){
-    // set current operator
-    if(this.innerText === 'x'){
-      currentOperator = '*';
+    if(!currentOperator){
+      firstNumber = readout.innerText;
+      
+      // set current operator
+      if(this.innerText === 'x'){
+        currentOperator = '*';
+      }
+      else {
+        currentOperator = this.innerText;
+      }
+  
+      operatorJustClicked = true;
     }
-    else {
-      currentOperator = this.innerText;
+    else{ // operator has already been selected
+
     }
-    readout.innerText += currentOperator;
   })
 })
 
 equalsBtn.addEventListener('click', function(){
-  readout.innerText = eval(readout.innerText);
+  // using evaluate() instead of eval()
+  secondNumber = readout.innerText;
+  readout.innerText = evaluate(currentOperator, Number(firstNumber), Number(secondNumber));
 });
+
+// a clear button
+// being able to chain operations
